a55ba9b496a9b8624fdedeae54943c9227f8f6f0
davidtadams <adams.t.david@gmail.com>
2016-08-09 13:41:17 -0600

    adding more annotations options

--- RestSample/src/java/resources/GenericResource.java
+++ RestSample/src/java/resources/GenericResource.java
@@ -1,21 +1,40 @@
 package resources;
 
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
-@Path("generic")
+@Path("/generic")
 public class GenericResource {
 
     @GET
-    public String sayHello() {
-        return "Hello hello hello";
+    @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello(
+            @DefaultValue("12345")
+            @QueryParam("zipcode") String zipcode,
+            @HeaderParam("x-name") String name
+    ) {
+        return "Hello, " + name + "  your zip code is: " + zipcode;
+    }
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHello2() {
+        return "<html><head><body>Testing";
     }
     
     @GET
-    @Path("extra")
-    public String sayExtra() {
-        return "some extra stuff";
+    @Path("extra/{thePrimaryKey}")
+    public String sayExtra(@PathParam("thePrimaryKey") String pk) {
+        return String.format("Here is the key %s", pk);
     }
     
     @DELETE
